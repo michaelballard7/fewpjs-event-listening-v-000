de768c9573c28a3e071727e74ae2ba4f0ebb7053
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 17:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // grab the input
 const input = document.querySelector('input')
 
-input.addEventListener('click', function (e){
-    console.log('clicked')
-})
 
 // add an event listener
+function addingEventListener() {
+
+    input.addEventListener('click',function(e){
+        console.log(e)
+    })
+}
