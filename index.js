5de13d462dda0514765b9b2337b24869cf96b9a4
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 17:52:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
+// grab the input
+const input = document.querySelector('input')
+
+
+// add an event listener
 function addingEventListener() {
+
+    input.addEventListener('click',function(e){
+        console.log(e)
+    })
 }
